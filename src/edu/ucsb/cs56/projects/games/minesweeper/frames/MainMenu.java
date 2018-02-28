501db8ddb717a47dbb8dfcdb8ab671e1a04849bc
Matt Gottlieb <Matt@Matts-Macbook-Pro.local>
2018-02-28 00:42:09 -0800

    Game now displays MineSweeper on top of the window. Added Customized Background to the Main Menu Screen

--- src/edu/ucsb/cs56/projects/games/minesweeper/frames/MainMenu.java
+++ src/edu/ucsb/cs56/projects/games/minesweeper/frames/MainMenu.java
@@ -1,12 +1,35 @@
 package edu.ucsb.cs56.projects.games.minesweeper.frames;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import java.awt.Image;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+
+
+
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
-
+//import java.awt.image.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+/*
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel; //*
+import javax.swing.JLabel;
+import javax.swing.ImageIo;
+*/
 
 import edu.ucsb.cs56.projects.games.minesweeper.constants.Constants;
 import edu.ucsb.cs56.projects.games.minesweeper.gui.MineGUI;
@@ -17,8 +40,9 @@ import edu.ucsb.cs56.projects.games.minesweeper.gui.MineGUI;
  * @author Kate Perkins
  */
 
-public class MainMenu extends JFrame {
 
+
+public class MainMenu extends JFrame {
 	private JButton quitMine;
 	private JButton easyGame;
 	private JButton medGame;
@@ -27,15 +51,42 @@ public class MainMenu extends JFrame {
 	private JButton help;    //Main Menu Help Button
 	private JButton highScore; // this label status displays the local high score.
 	
+	private JPanel panel2;
+	private JLabel lblBackgroundImage = new JLabel();
 	/**
 	 * Default Constructor for main menu
 	 * @throws HeadlessException if no display
 	 */
 	public MainMenu() throws HeadlessException {
-		super();
-		setSize(650, 600);
-		Container menu = getContentPane();
-		menu.setLayout(new GridLayout(7, 0)); //our 2 section grid layout for our main menu
+		setTitle("MineSweeper");
+		setSize(1000, 800);
+		//super();
+
+		panel2 = new JPanel();
+		panel2.setOpaque(false);
+		panel2.setLayout(new FlowLayout());
+
+		
+
+
+		ImageIcon imageIcon = new ImageIcon("screenshots/MINESWEEPERMenuScreen.png"); // load the image to a imageIcon
+		Image image = imageIcon.getImage(); // transform it 
+		Image newimg = image.getScaledInstance(1000, 800,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+		imageIcon = new ImageIcon(newimg); 
+		lblBackgroundImage.setLayout(new FlowLayout());
+		lblBackgroundImage.setIcon(imageIcon);
+
+
+
+
+
+
+		//add(new ContentPanel());
+		
+		setVisible(true);
+		setLocation(200, 200);
+		//Container menu = getContentPane();
+		//menu.setLayout(new GridLayout(7, 0)); //our 2 section grid layout for our main menu
 		quitMine = new JButton("Quit Minesweeper");
 		easyGame = new JButton("New Easy Game");
 		medGame = new JButton("New Medium Game");
@@ -43,6 +94,8 @@ public class MainMenu extends JFrame {
 		help = new JButton("Help");
 		load = new JButton("Load Last Game");
 		highScore = new JButton("Leaderboards");
+
+
 		easyGame.addActionListener((ActionEvent e) -> {
 			if (MineGUI.overwriteSavePrompt()) {
 				MineGUI.newGame(Constants.Difficulty.EASY);
@@ -62,6 +115,43 @@ public class MainMenu extends JFrame {
 		load.addActionListener((ActionEvent e) -> { MineGUI.newGame(Constants.Difficulty.LOAD); });
 		quitMine.addActionListener((ActionEvent e) -> { MineGUI.quitPrompt(); });
 		highScore.addActionListener((ActionEvent e) -> {MineGUI.setLeaderboardVisible(true); });
+		
+
+
+
+
+		panel2.add(quitMine);
+	
+		panel2.add(easyGame);
+		
+		panel2.add(medGame);
+		
+		panel2.add(hardGame);
+	
+		panel2.add(load);
+	
+		panel2.add(help);
+	
+		panel2.add(highScore);
+	
+		add(panel2);
+
+		lblBackgroundImage.add(panel2);
+
+		add(lblBackgroundImage);
+
+		//menu.add(panel);
+
+		//ImageIcon icon= new ImageIcon("screenshots/MINESWEEPERMenuScreen.png");
+		//JLabel backgroundPhoto= new JLabel();
+		//backgroundPhoto.setIcon(icon);
+
+		
+		//JLabel contentPane = new JLabel();
+		//contentPane.setIcon(new ImageIo("screenshots/MINESWEEPERMenuScreen.png"));
+		//contentPane.setLayout(new BorderLayout());
+		//menu.setContentPane(contentPane);
+/*
 		menu.add(easyGame);
 		menu.add(medGame);
 		menu.add(hardGame);
@@ -69,9 +159,27 @@ public class MainMenu extends JFrame {
 		menu.add(help);
 		menu.add(quitMine);
 		menu.add(highScore); // add new highScore feature to frame.
+		*/
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 	}
+
+
+/*
+	private Image getScaledImage(Image srcImg, int w, int h){
+    	BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+    	Graphics2D g2 = resizedImg.createGraphics();
+
+    	g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+    	g2.drawImage(srcImg, 0, 0, w, h, null);
+    	g2.dispose();
+
+    	return resizedImg;
+	}
+
+*/
+
+
 
 	public int getEasyGameX() {
 		return easyGame.getX();
